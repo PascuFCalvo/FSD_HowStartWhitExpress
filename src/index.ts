6d0d8dc3a25b1e5ff0b65fade4f4c535d622b612
PascuFCalvo <pascual.fernandez.calvo@gmail.com>
2023-10-18 13:15:28 +0200

    feat:crud fimls

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,36 @@ import express from "express";
 //ejecutar express y guardalo en la constante app
 const app = express();
 
+//empezar a crear la logica
+   //crear primera url del backend (endpoints)
+
+app.get("/films" , (req, res) => {
+   //logica de lo que quieras hacer/devolver
+   return res.send("GET films");
+})
+
+//otro endpoint
+
+app.post("/films", (req, res)=>{
+   //logica para crear peliculas
+   return res.send("POST films");
+})
+
+app.put("/films", (req, res)=>{
+   //logica para crear peliculas
+   return res.send("UPDATE films");
+})
+
+app.delete("/films", (req, res)=>{
+   //logica para crear peliculas
+   return res.send("DELETE films");
+})
+
+
+
 //habilita un puerto donde lo dejas escuchando, primer parametro un puerto y como segundo la funcion callback
 app.listen(3000, () => {
    console.log("Server running");
    
 })
+
