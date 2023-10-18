72ee3bb565f9073962906ca020f360188625430b
PascuFCalvo <pascual.fernandez.calvo@gmail.com>
2023-10-18 13:36:50 +0200

    feat: req params to the endoints

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 // crear un servidor de express
-
 //importar express
 import express from "express";
 
@@ -9,29 +8,48 @@ const app = express();
 //empezar a crear la logica
    //crear primera url del backend (endpoints)
 
-app.get("/films" , (req, res) => {
+   // /:id es el parametro dinamico de la ruta , da igual que tengas 1 o 2000 ids que se va concatendo
+
+app.get("/films/:id" , (req, res) => {
    //logica de lo que quieras hacer/devolver
+
+   //como pintar en consola el parametro que hemos metido en la ruta despues de films/ (en la consulta en el thunderclient)
+   console.log(req.params)
+   //como es un objeto podemos acceder a su id
+   console.log(req.params.id)
+   //como ya sabemos como recuperar , lo guardamos en una constante
+   const filmsId = req.params.id
+
+   console.log("get")
    return res.send("GET films");
+   
 })
 
-//otro endpoint
+//seguimos creando el resto de endpoints
 
-app.post("/films", (req, res)=>{
+app.post("/films/:id", (req, res)=>{
    //logica para crear peliculas
+   const filmId = req.params.id;
+   console.log ("posted film " + filmId)
+   console.log("post")
    return res.send("POST films");
 })
 
-app.put("/films", (req, res)=>{
-   //logica para crear peliculas
+app.put("/films/:id", (req, res)=>{
+   //logica para editar peliculas
+   const filmId = req.params.id;
+   console.log ("updated film " + filmId)
+   console.log("send")
    return res.send("UPDATE films");
 })
 
-app.delete("/films", (req, res)=>{
-   //logica para crear peliculas
+app.delete("/films/:id", (req, res)=>{
+   //logica para borrar peliculas
+   const filmId = req.params.id;
+   console.log ("deleted film " + filmId)
+   console.log("delete")
    return res.send("DELETE films");
 })
-
-
 
 //habilita un puerto donde lo dejas escuchando, primer parametro un puerto y como segundo la funcion callback
 app.listen(3000, () => {
