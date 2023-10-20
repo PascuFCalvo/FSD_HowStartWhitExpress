feb3bad9b84670ab854ad496d2701e1a8aefc537
PascuFCalvo <pascual.fernandez.calvo@gmail.com>
2023-10-20 12:05:36 +0200

    feat: table migration

--- database.ts
+++ database.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata"
 
 import { DataSource } from "typeorm"
+import { Film1697789907147 } from "./src/migration/1697789907147-film"
 
 // le metes el export en la misma linea directamente que seria lo mismo que poner: export { AppDataSource } al final
 
@@ -11,6 +12,8 @@ export const AppDataSource = new DataSource({
  username: "root",
  password: "karanas3689",
  database: "fsd-typeorm",
+ //añadimos las migraciones que tiene que lanzar
+ migrations:[Film1697789907147],
  entities: [],
  synchronize: false,
  logging: false,
