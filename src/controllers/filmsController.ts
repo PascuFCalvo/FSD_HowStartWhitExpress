2359734832182e79ecabde0e11336e6dcfbe3493
PascuFCalvo <pascual.fernandez.calvo@gmail.com>
2023-10-20 13:29:10 +0200

    feat:delete films

--- src/controllers/filmsController.ts
+++ src/controllers/filmsController.ts
@@ -3,10 +3,14 @@ import { Request, Response } from "express";
 import { Film } from "../models/Film";
 
 const getAllFilms = async (req: Request, res: Response) => {
-  const films = await Film.find();
+  try {
+    const films = await Film.find();
 
-  console.log("get");
-  return res.send(films);
+    console.log("get");
+    return res.send(films);
+  } catch (error) {
+    return res.send(error);
+  }
 };
 
 const getAFilmById = (req: Request, res: Response) => {
@@ -43,12 +47,26 @@ const modifyAFilmById = (req: Request, res: Response) => {
   return res.send("UPDATE films");
 };
 
-const deleteAFilmById = (req: Request, res: Response) => {
-  //logica para borrar peliculas
-  const filmId = req.params.id;
-  console.log("deleted film " + filmId);
-  console.log("delete");
-  return res.send("DELETE films");
+const deleteAFilmById = async (req: Request, res: Response) => {
+  //otro mas facil
+
+  // Film.delete({
+  //  id:parseInt(filmIdToDelete)
+  // })
+  //  va a devolver un 0 o un 1 si ha borrado o no , no puedes averiguar que has borrado
+  //
+  try {
+    const filmIdToDelete = req.params.id;
+
+    const filmToRemove = await Film.findOneBy({
+      id: parseInt(filmIdToDelete),
+    });
+
+    let filmRemoved = await Film.remove(filmToRemove as Film); // hay que ponerle un as Film para que no chille typescript
+    return res.send(filmRemoved);
+  } catch (error) {
+    return res.send(error);
+  }
 };
 
 export {
